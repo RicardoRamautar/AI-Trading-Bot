0fc1967b2e2724a72a1ae9aeaa1920958652e6d1
Ricardo Ramautar <ricardo.ramautar@gmail.com>
2024-07-30 18:56:53 +0200

    Add buy and sell functions

--- environment.py
+++ environment.py
@@ -4,6 +4,7 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
+from Trade import Trade
 from TradeDatabase import TradeDatabase
 
 # Global variables
@@ -25,16 +26,19 @@ class BTC:
                  commission = COMMISSION):
         
         self.start_date = start_date
-        self.end_date = end_date
-        self.interval = interval
-        self.t = 0
-        self.assets = assets
-        self.fee = fee
+        self.end_date   = end_date
+        self.interval   = interval
+        self.t          = 0
+        self.assets     = assets
+        self.fee        = fee
         self.commission = commission
 
         self.btc_prices, self.dates = self.get_price_data()
 
         self.trade_history = TradeDatabase()
+
+    def reset(self):
+        self.__init__()
 
     def get_price_data(self):
         btc = yf.download("BTC-USD", 
@@ -52,3 +56,31 @@ class BTC:
     
     def get_price(self, t):
         return self.btc_prices[t]
+    
+    def buy(self, date):
+        price = self.get_price(date)
+        trade = Trade(price, self.fee, self.commission, date)
+
+        trade.buy()
+        
+        if trade.buy_value > self.assets:
+            print("ERROR: Cannot spend more than you have!")
+            return -1
+        elif self.trade_history:
+            print("ERROR: Can only hold one trade at a time!")
+            return -1
+        else:
+            self.trade_history.add_open_trade(trade)
+            assets -= trade.cost
+    
+    def sell(self):
+        if not self.trade_history:
+            print("ERROR: You have no open trades to sell!")
+            return -1
+        else:
+           reward = self.trade_history.close_trade(self.trade_history.open_trade.buy_date)
+
+           return reward
+
+
+    

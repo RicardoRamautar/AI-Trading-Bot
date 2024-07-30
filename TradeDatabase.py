8f7435d2e547074e0a7dfad475560cad4452471b
Ricardo Ramautar <ricardo.ramautar@gmail.com>
2024-07-30 19:17:40 +0200

    Only allow one open trade

--- TradeDatabase.py
+++ TradeDatabase.py
@@ -2,12 +2,12 @@ from Trade import Trade
 
 class TradeDatabase:
     def __init__(self):
-        self.open_trades    = {}
+        self.open_trades    = None
         self.closed_trades  = {}
 
     def add_open_trade(self, trade):
-        self.open_trades[trade.buy_date] = trade
+        self.open_trade = trade
 
-    def add_closed_trade(self, trade):
-        del self.open_trades[trade.buy_date]
+    def close_trade(self, trade):
+        self.open_trade = None
         self.closed_trades[trade.buy_date] = trade
